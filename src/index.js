89b4c8419b0c98e0ed8cc17ff299381f173e1add
cfyz7 <lu5294677@gmail.com>
2022-10-23 16:44:30 +0600

    change index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-#!/usr/bin/env node
-//logic of games
 import readlineSync from 'readline-sync';
-import uniqQuestion from '../bin/brain-prime.js'; 
-
+import { uniqQuestion, reviewResult } from '../games/prime.js';
+ 
 const sayHello = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`); 
-  uniqQuestion(); 
-  // for (let i = 1; i <= 3; i += 1) {
-    
-  //   if (obj.answer != cAnswer) {
-  //     console.log(`${aaaaa} is wrong answer ;(. Correct answer was '${aaaaa}'.\nLet's try again, ${userName}`);
-  //     return;
-  //   }
-  //   console.log('Correct!');
-  // }
-  // console.log(`Congratulations, ${userName}!`);
+  console.log(`Hello, ${userName}!`);
+  console.log(uniqQuestion())  
+  for (let i = 1; i <= 3; i += 1) { 
+    const [question, result] = reviewResult();
+    console.log(`Question: ${question}`); 
+    const answerOfUser = readlineSync.question('Your answer: '); 
+    if (answerOfUser != result) {
+      console.log(`'${answerOfUser}' is wrong answer ;(. Correct answer was '${result}'.\nLet's try again, ${userName}`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congretulations, ${userName}!`)
 };
-
-export default sayHello; 
+export default sayHello;
