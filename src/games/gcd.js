cc6ddcd158232731b1854eacf21a0c407ac94afe
cfyz7 <lu5294677@gmail.com>
2022-11-15 08:27:23 +0600

    correct gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,18 +4,18 @@ import gamesForMind from '../index.js';
 
 export const uniqQuestion = () => 'Find the greatest common divisor of given numbers.';
 
-const reviewNumber = function(num1, num2) {
+const reviewNumber = function (num1, num2) {
   if (!num2) {
     return num1;
-  } 
+  }
   return reviewNumber(num2, num1 % num2);
-}
+};
 
 const reviewResult = () => {
-  const num1 = getRandomNumber(1, 100)
-  const num2 = getRandomNumber(1, 100) 
+  const num1 = getRandomNumber(1, 100);
+  const num2 = getRandomNumber(1, 100);
   const question = `${num1} ${num2}`;
-  const result = String(reviewNumber(num1, num2))
+  const result = String(reviewNumber(num1, num2));
   return [question, result];
 };
 
